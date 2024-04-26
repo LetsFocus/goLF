7be973793fb62e1693e82511d1bccaa9939cea74
Durga Chikkala <69903252+Durga-chikkala@users.noreply.github.com>
2024-04-26 18:10:01 +0530

    Create enum.go

---
+++ errors/enum.go
@@ -0,0 +1,11 @@
+package errors
+
+// EnumError represents a 400 Bad Request error indicating an invalid enumeration value.
+type EnumError struct {
+	SupportedValues []string
+	Value           string
+}
+
+func (e *EnumError) Error() string {
+	return fmt.Sprintf("Value of the field %s must be one of [%s]", e.Value, strings.Join(e.SupportedValues, ", "))
+}
